a32ea7c7e53566c9e00314f1494453dfad54fd3e
habibwisnup <mtc.altair@gmail.com>
2025-01-11 21:55:11 +0700

    implement functional login app atm

--- src/main/java/org/habibwisnup/Main.java
+++ src/main/java/org/habibwisnup/Main.java
@@ -1,7 +1,31 @@
 package org.habibwisnup;
 
+import org.habibwisnup.factory.Command;
+import org.habibwisnup.factory.CommandFactory;
+import org.habibwisnup.factory.CommandInvoker;
+import org.habibwisnup.managers.CustomerManager;
+import org.habibwisnup.utils.errorHandler.ErrorHandler;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CustomerManager customerManager = new CustomerManager();
+        CommandFactory commandFactory = new CommandFactory(customerManager);
+        CommandInvoker commandInvoker = new CommandInvoker();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                String[] parts = input.split(" ");
+                Command command = commandFactory.getCommand(parts);
+                commandInvoker.execute(command);
+            } catch (Exception e) {
+                ErrorHandler.handleException(e);
+            }
+        }
     }
 }
--- src/main/java/org/habibwisnup/managers/CustomerManager.java
+++ src/main/java/org/habibwisnup/managers/CustomerManager.java
@@ -1,5 +1,6 @@
 package org.habibwisnup.managers;
 
+import org.habibwisnup.models.Customer;
 import org.habibwisnup.services.impl.CustomerService;
 import org.habibwisnup.utils.MessageConstant;
 import org.habibwisnup.utils.errorHandler.exceptions.GeneralException;
@@ -10,6 +11,27 @@ import java.util.Map;
 public class CustomerManager {
     private CustomerService currentCustomer;
     private final Map<String, CustomerService> customers = new HashMap<>();
+
+    public void login(String name) {
+        if (currentCustomer != null) {
+            System.out.println(MessageConstant.USER_ALREADY_LOGIN);
+            return;
+        }
+        CustomerService customerService = customers.computeIfAbsent(name, k -> new CustomerService(new Customer(name)));
+        currentCustomer = customerService;
+
+        System.out.println("Hello, " + currentCustomer.getCustomer().getName() + "!");
+        System.out.println("Your balance is $" + currentCustomer.getCustomer().getBalance());
+    }
+
+    public void logout() {
+        if (currentCustomer == null) {
+            System.out.println(MessageConstant.USER_NOT_LOGIN_MESSAGE);
+            return;
+        }
+        System.out.println("Goodbye, " + currentCustomer.getCustomer().getName() + "!");
+        currentCustomer = null;
+    }
 
     public CustomerService getCurrentCustomer() {
         if (currentCustomer == null) throw new GeneralException(MessageConstant.USER_NOT_LOGIN_MESSAGE);

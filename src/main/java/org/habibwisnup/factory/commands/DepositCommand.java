2dd71f62ef61f4f3baf9c685ccc80ea63728efae
habibwisnup <mtc.altair@gmail.com>
2025-01-12 01:32:02 +0700

    fix deposit logic

--- src/main/java/org/habibwisnup/factory/commands/DepositCommand.java
+++ src/main/java/org/habibwisnup/factory/commands/DepositCommand.java
@@ -2,6 +2,7 @@ package org.habibwisnup.factory.commands;
 
 import org.habibwisnup.factory.Command;
 import org.habibwisnup.managers.CustomerManager;
+import org.habibwisnup.utils.MessageConstant;
 
 public class DepositCommand implements Command {
     private CustomerManager customerManager;
@@ -22,7 +23,34 @@ public class DepositCommand implements Command {
 
     @Override
     public void execute() {
+        var debts =customerManager.getCurrentCustomer().getCustomer().getDebtors();
         customerManager.getCurrentCustomer().deposit(amount);
-        System.out.println("Your balance is $" + customerManager.getCurrentCustomer().getCustomer().getBalance());
+        if(debts.isEmpty()) {
+            System.out.println("Your balance is $" + customerManager.getCurrentCustomer().getCustomer().getBalance());
+        }
+        else {
+            TransferCommand transferCommand = new TransferCommand(customerManager);
+            var totalDebtPaid = 0;
+
+            for (var kvp : debts.entrySet()) {
+                String debtor = kvp.getKey();
+                Integer debt = kvp.getValue();
+
+                int amountToPay = Math.min(debt, amount - totalDebtPaid);
+
+                var transferArgs = new String[]{
+                        MessageConstant.TRANSFER_COMMAND, debtor, String.valueOf(amountToPay)
+                };
+
+                transferCommand.initialize(transferArgs).execute();
+                totalDebtPaid += amountToPay;
+            }
+        }
+
+        for (var kvp : debts.entrySet()) {
+            if (kvp.getValue() == 0) {
+                customerManager.getCurrentCustomer().getCustomer().getDebtors().remove(kvp.getKey());
+            }
+        }
     }
 }
